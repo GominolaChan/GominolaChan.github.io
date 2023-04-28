ce798db9bafd554290e255a284355b50edd18fd9
JosueED <107262855+GominolaChan@users.noreply.github.com>
2023-04-28 19:29:57 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,34 +1,56 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var snake = [{x: 0, y: 0}];
-var direction = "right";
+// obtén el elemento canvas
+var canvas = document.getElementById("miCanvas");
 
-function drawSnake() {
+// crea el contexto 2D
+var ctx = canvas.getContext("2d");
+
+// establece la posición inicial
+var x = canvas.width/2;
+var y = canvas.height/2;
+
+// establece la velocidad inicial
+var dx = 2;
+var dy = -2;
+
+// establece el radio del círculo
+var ballRadius = 10;
+
+// función para dibujar el círculo
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// función para mover el círculo
+function moveBall() {
+  // borra el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#000000";
-  for (var i = 0; i < snake.length; i++) {
-    ctx.fillRect(snake[i].x, snake[i].y, 10, 10);
+  
+  // dibuja el círculo
+  drawBall();
+  
+  // mueve el círculo
+  x += dx;
+  y += dy;
+  
+  // cambia la dirección si el círculo llega a los bordes del canvas
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    dy = -dy;
   }
 }
 
-function moveSnake() {
-  var head = {x: snake[0].x, y: snake[0].y};
-  if (direction === "right") head.x += 10;
-  else if (direction === "left") head.x -= 10;
-  else if (direction === "up") head.y -= 10;
-  else if (direction === "down") head.y += 10;
-  snake.unshift(head);
-  snake.pop();
-}
+// llama a la función moveBall cada 10 milisegundos para crear la animación
+setInterval(moveBall, 10);
+Este código utiliza el elemento canvas de HTML5 y el contexto 2D para dibujar un círculo que se mueve aleatoriamente por la pantalla. La función drawBall() dibuja el círculo en la posición actual, mientras que la función moveBall() borra el canvas, dibuja el círculo en la nueva posición y cambia la dirección si llega a los bordes del canvas. Finalmente, la función setInterval() llama a moveBall() cada 10 milisegundos para crear la animación.
 
-document.addEventListener("keydown", function(event) {
-  if (event.keyCode === 39 && direction !== "left") direction = "right";
-  else if (event.keyCode === 37 && direction !== "right") direction = "left";
-  else if (event.keyCode === 38 && direction !== "down") direction = "up";
-  else if (event.keyCode === 40 && direction !== "up") direction = "down";
-});
 
-setInterval(function() {
-  moveSnake();
-  drawSnake();
-}, 100);
+
+
+
+
