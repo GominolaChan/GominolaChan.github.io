5d819d63f3338eb7142b970207287bcc557eda7e
JosueED <107262855+GominolaChan@users.noreply.github.com>
2023-04-28 20:14:31 +0200

    Update serpiente.js

--- serpiente.js
+++ serpiente.js
@@ -1,29 +1,51 @@
-var container = document.getElementById("container");
+var juego = document.getElementById("juego");
+var segmentos = [{x: 10, y: 10}, {x: 10, y: 15}, {x: 10, y: 20}]; // Posiciones iniciales de la serpiente
+var direccion = "abajo";
+var movimiento;
 
-// Definimos la serpiente
-var snake = [];
-snake[0] = {x: 250, y: 250};
-var snakeLength = 5;
+function actualizarJuego() {
+  // Mover la serpiente
+  var cabeza = segmentos[0];
+  switch (direccion) {
+    case "arriba":
+      cabeza = {x: cabeza.x, y: cabeza.y - 5};
+      break;
+    case "abajo":
+      cabeza = {x: cabeza.x, y: cabeza.y + 5};
+      break;
+    case "izquierda":
+      cabeza = {x: cabeza.x - 5, y: cabeza.y};
+      break;
+    case "derecha":
+      cabeza = {x: cabeza.x + 5, y: cabeza.y};
+      break;
+  }
+  segmentos.unshift(cabeza);
+  segmentos.pop();
 
-// Movemos la serpiente aleatoriamente cada 100ms
-setInterval(function() {
-    // Mover la serpiente
-    for (var i = snake.length - 1; i > 0; i--) {
-        snake[i].x = snake[i-1].x;
-        snake[i].y = snake[i-1].y;
-    }
-    snake[0].x += Math.floor(Math.random() * 3) - 1;
-    snake[0].y += Math.floor(Math.random() * 3) - 1;
-    
-    // Limpiar el contenedor
-    container.innerHTML = "";
-    
-    // Dibujar la serpiente
-    for (var i = 0; i < snake.length; i++) {
-        var div = document.createElement("div");
-        div.className = "snake";
-        div.style.left = snake[i].x + "px";
-        div.style.top = snake[i].y + "px";
-        container.appendChild(div);
-    }
-}, 100);
+  // Actualizar la posición de los segmentos en la pantalla
+  for (var i = 0; i < segmentos.length; i++) {
+    var segmento = document.getElementById("segmento" + i);
+    segmento.style.left = segmentos[i].x + "px";
+    segmento.style.top = segmentos[i].y + "px";
+  }
+
+  // Comprobar si la serpiente ha chocado con las paredes
+  var cabezaX = segmentos[0].x;
+  var cabezaY = segmentos[0].y;
+  if (cabezaX < 0 || cabezaX >= 400 || cabezaY < 0 || cabezaY >= 400) {
+    clearInterval(movimiento);
+    alert("Game over!");
+  }
+}
+
+// Iniciar el movimiento de la serpiente
+movimiento = setInterval(actualizarJuego, 25);
+
+// Cambiar la dirección de la serpiente cuando se pulsan las teclas de dirección
+document.onkeydown = function(event) {
+  switch (event.keyCode) {
+    case 38: // Tecla de flecha hacia arriba
+      direccion = "arriba";
+      break;
+    case 40: // Tecla de flecha hacia abajo
