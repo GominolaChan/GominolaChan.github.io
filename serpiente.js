0f31f0c02db18bb2b24b52ba4808707fb6c247f8
JosueED <107262855+GominolaChan@users.noreply.github.com>
2023-04-28 20:20:20 +0200

    Update serpiente.js

--- serpiente.js
+++ serpiente.js
@@ -1,30 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// set up snake initial position and size
-let snake = [{ x: 0, y: 0 }, { x: 5, y: 0 }, { x: 10, y: 0 }];
-let snakeSize = 5;
+const snakeSize = 5;
+let snake = [{ x: 10, y: 10 }];
 
-// set up initial velocity
-let velocity = { x: 1, y: 0 };
-
-// set up game loop
-setInterval(() => {
-  // update snake position
-  for (let i = snake.length - 1; i > 0; i--) {
-    snake[i].x = snake[i - 1].x;
-    snake[i].y = snake[i - 1].y;
-  }
-
-  snake[0].x += velocity.x * snakeSize;
-  snake[0].y += velocity.y * snakeSize;
-
-  // clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // draw snake
-  ctx.fillStyle = "green";
+function drawSnake() {
   for (let i = 0; i < snake.length; i++) {
     ctx.fillRect(snake[i].x, snake[i].y, snakeSize, snakeSize);
   }
-}, 1000 / 30); // update at 30 fps
+}
+
+function moveSnake() {
+  let dx = snakeSize;
+  let dy = 0;
+  snake.unshift({ x: snake[0].x + dx, y: snake[0].y + dy });
+  snake.pop();
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function gameLoop() {
+  clearCanvas();
+  moveSnake();
+  drawSnake();
+}
+
+setInterval(gameLoop, 100);
